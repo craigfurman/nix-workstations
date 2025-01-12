c773106d21ee90869c7c371630c549a4b88254a6
Craig Furman <craig@craigfurman.dev>
2025-01-12 15:44:38 +0000

    autokbisw service

--- darwin/autokbisw.nix
+++ darwin/autokbisw.nix
@@ -1,4 +1,14 @@
 { pkgs, ... }:
 {
   environment.systemPackages = [ pkgs.autokbisw ];
+
+  launchd.user.agents.autokbisw = {
+    command = "${pkgs.autokbisw}/bin/autokbisw";
+    serviceConfig = {
+      KeepAlive = true;
+      RunAtLoad = true;
+      StandardOutPath = "/tmp/autokbisw.log";
+      StandardErrorPath = "/tmp/autokbisw.log";
+    };
+  };
 }
