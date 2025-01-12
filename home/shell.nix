217e7780c26a0214fc99aa978db2ffe4821b626a
Craig Furman <craig@craigfurman.dev>
2025-01-12 16:40:09 +0000

    tinted-shell colorscheme

--- home/shell.nix
+++ home/shell.nix
@@ -1,3 +1,10 @@
+let
+  tintedShellRepo = builtins.fetchGit {
+    url = "https://github.com/tinted-theming/tinted-shell.git";
+    ref = "main";
+    rev = "839f96d22a5b3a702444b2b19513b2e751ff748a";
+  };
+in
 {
   home.sessionVariables = {
     EDITOR = "nvim";
@@ -8,6 +15,11 @@
 
     # Keep effectively unlimited history
     history.save = 1000000000;
+
+    initExtra = ''
+      source ${tintedShellRepo}/profile_helper.sh
+      base16_gruvbox-dark-medium
+    '';
 
     oh-my-zsh = {
       enable = true;
