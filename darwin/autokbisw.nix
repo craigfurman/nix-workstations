2f310c53b6dca799b6d32ea6d18c0e159952f440
Craig Furman <craig@craigfurman.dev>
2025-01-12 15:47:33 +0000

    Clarify manual step

--- darwin/autokbisw.nix
+++ darwin/autokbisw.nix
@@ -2,6 +2,8 @@
 {
   environment.systemPackages = [ pkgs.autokbisw ];
 
+  # Currently, giving autokbisw the ability to monitor keyboard input is a
+  # manual step. I don't think nix-darwin supports this yet.
   launchd.user.agents.autokbisw = {
     command = "${pkgs.autokbisw}/bin/autokbisw";
     serviceConfig = {
