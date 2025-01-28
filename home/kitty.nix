98e6b9d9cd9f87ae2b5da7a066d6fba3ab6aa935
Craig Furman <craig@craigfurman.dev>
2025-01-28 22:05:50 +0000

    Move kitty config to nix rather than a file

--- home/kitty.nix
+++ home/kitty.nix
@@ -2,6 +2,29 @@
   programs.kitty = {
     enable = true;
 
-    extraConfig = builtins.readFile ./kitty.conf;
+    font = {
+      name = "Inconsolata Nerd Font Mono";
+      size = 14;
+    };
+
+    keybindings = {
+      "ctrl+shift+n" = "new_os_window_with_cwd";
+      "ctrl+shift+t" = "new_tab_with_cwd";
+      "ctrl+shift+enter" = "launch --cwd=current --location=split";
+      "ctrl+shift+f4" = "launch --cwd=current --location=split";
+      "ctrl+shift+f5" = "launch --cwd=current --location=hsplit";
+      "ctrl+shift+f6" = "launch --cwd=current --location=vsplit";
+      "ctrl+shift+f7" = "layout_action rotate";
+    };
+
+    settings = {
+      cursor_blink_interval = 0;
+      copy_on_select = true;
+      enabled_layouts = "splits,stack";
+      hide_window_decorations = true;
+      confirm_os_window_close = 0;
+      macos_option_as_alt = "left";
+      macos_quit_when_last_window_closed = true;
+    };
   };
 }
