e7c464893a15774d6d807983a69a3c0f2e203904
Craig Furman <craig@craigfurman.dev>
2025-03-26 20:55:57 +0000

    gnome module
    attempt to configure system tray icons

--- nixos/chargin-chuck/configuration.nix
+++ nixos/chargin-chuck/configuration.nix
@@ -11,6 +11,7 @@
   imports = [
     ../base
     ./games.nix
+    ./gnome.nix
     ./hardware-configuration.nix
   ];
 
@@ -45,10 +46,6 @@
   };
 
   services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -99,10 +96,6 @@
   # Enable automatic login for the user.
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "craig";
-
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
