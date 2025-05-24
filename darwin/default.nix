2b8f03daa96a74ccd7143a087040afae68e93354
Craig Furman <craig@craigfurman.dev>
2025-05-24 12:56:17 +0100

    Refactor nix-darwin

--- darwin/default.nix
+++ darwin/default.nix
@@ -1,50 +1,22 @@
 {
-  config,
   flake,
   overlay,
   system,
   ...
 }:
 {
-  system.configurationRevision = flake.rev or flake.dirtyRev or null;
-
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
-  system.stateVersion = 5;
-
-  nixpkgs.hostPlatform = system;
-
-  nixpkgs.overlays = [ overlay ];
-
   imports = [
     ./apps.nix
     ./manage-other-machines.nix
+    ./nix.nix
     ./settings.nix
   ];
 
-  environment.etc.nix-darwin.source = "${
-    config.users.users.${config.system.primaryUser}.home
-  }/.config/nix-darwin";
-
-  nix = {
-    # Clean up old darwin generations. I believe this will run as root. HM
-    # defines its own GC.
-    gc = {
-      automatic = true;
-      options = "--delete-older-than 14d";
-    };
-
-    # For building linux packages in a qemu VM. I don't want to leave this on
-    # all the time, so it is disabled usually.
-    linux-builder = {
-      enable = false;
-    };
-
-    settings = {
-      experimental-features = "nix-command flakes";
-      trusted-users = [ config.system.primaryUser ];
-    };
-  };
+  nixpkgs.hostPlatform = system;
+  nixpkgs.overlays = [ overlay ];
 
   system.primaryUser = "craig";
+
+  system.configurationRevision = flake.rev or flake.dirtyRev or null;
+  system.stateVersion = 5;
 }
