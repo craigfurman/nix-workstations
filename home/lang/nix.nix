a10c5dbe6cd4b08c31f805a1278dfec070528829
Craig Furman <git@craigfurman.dev>
2025-11-21 10:53:11 +0000

    nil_ls -> nixd

--- home/lang/nix.nix
+++ home/lang/nix.nix
@@ -5,15 +5,11 @@
   programs.neovim = {
     craigExtensions.treesitterParsers = [ "nix" ];
 
-    extraConfig =
-      let
-        extensions = [ "nix" ];
-      in
-      ''
-        ${craigLib.neovim.preSaveCommand extensions "lua vim.lsp.buf.format({timeout_ms=1000})"}
-      '';
+    extraConfig = ''
+      ${craigLib.neovim.preSaveCommand [ "nix" ] "lua vim.lsp.buf.format({timeout_ms=1000})"}
+    '';
 
-    extraLuaConfig = builtins.readFile ./nil-ls.lua;
-    extraPackages = [ pkgs.nil ];
+    extraLuaConfig = "vim.lsp.enable('nixd')";
+    extraPackages = [ pkgs.nixd ];
   };
 }
