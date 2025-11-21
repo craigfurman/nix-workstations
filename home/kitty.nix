963212459beea77dffe1e7316b21399a17c1c851
Craig Furman <git@craigfurman.dev>
2025-11-21 10:53:44 +0000

    formatting

--- home/kitty.nix
+++ home/kitty.nix
@@ -20,25 +20,24 @@
       "ctrl+shift+z" = "toggle_layout stack";
     };
 
-    settings =
-      {
-        confirm_os_window_close = 0;
-        copy_on_select = true;
-        cursor_blink_interval = 0;
-        enable_audio_bell = "no";
-        enabled_layouts = "splits,stack";
-        hide_window_decorations = true;
-        scrollback_lines = 20000;
-        scrollback_pager_history_size = 10; # MB
-      }
-      // (
-        if pkgs.stdenv.isDarwin then
-          {
-            macos_option_as_alt = "left";
-            macos_quit_when_last_window_closed = true;
-          }
-        else
-          { }
-      );
+    settings = {
+      confirm_os_window_close = 0;
+      copy_on_select = true;
+      cursor_blink_interval = 0;
+      enable_audio_bell = "no";
+      enabled_layouts = "splits,stack";
+      hide_window_decorations = true;
+      scrollback_lines = 20000;
+      scrollback_pager_history_size = 10; # MB
+    }
+    // (
+      if pkgs.stdenv.isDarwin then
+        {
+          macos_option_as_alt = "left";
+          macos_quit_when_last_window_closed = true;
+        }
+      else
+        { }
+    );
   };
 }
